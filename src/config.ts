f5551fcb1135a894c64e44e307734b198ee58fb2
Linglingfa <122542643+Liwu253874@users.noreply.github.com>
2024-10-22 20:35:46 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,10 @@ export let PORT = 370; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy' // 代理码密钥
 // 代理
-export let PROXYS: Proxy[] = [];
+export let PROXYS: Proxy[] = [ 
+  {
+        domain: "google.00fa.me",
+        url: "https://www.google.com/",
+        enable: true,
+        template: Google
+    }];
